4b9b6ca94fa25f732e70105aa56fb1224af09b66
Tim Foster <tfos262@wgu.edu>
2024-04-08 21:57:39 -0400

    add pictures, add form validation

---
+++ index.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const emailOne = document.getElementById("InputEmail1")
+const emailTwo = document.getElementById("InputEmail2")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+// html input will handle the emailOne input validation
+
+emailTwo.addEventListener("input", (event) => {
+    if (emailTwo.validity.typeMismatch) {
+        emailTwo.setCustomValidity("Not a valid email address!");
+    } else if (emailTwo.value !== emailOne.value){
+        emailTwo.setCustomValidity("Emails do not match!");
+    } else {
+        emailTwo.setCustomValidity("");
+    }
+  });
