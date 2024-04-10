bcbf0d02975c555e393f77ee5972d08cd5b604d6
Tim Foster <tfos262@wgu.edu>
2024-04-10 01:57:01 -0400

    update js and form

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const form = document.querySelector("form");
-const emailOne = document.getElementById("InputEmail1")
-const emailTwo = document.getElementById("InputEmail2")
+const form = document.getElementById("form");
+const emailOne = document.getElementById("InputEmail1");
+const emailTwo = document.getElementById("InputEmail2");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-})
+  event.preventDefault();
+});
 
 // html input will handle the emailOne input validation
 
 emailTwo.addEventListener("input", (event) => {
-    if (emailTwo.validity.typeMismatch) {
-        emailTwo.setCustomValidity("Not a valid email address!");
-    } else if (emailTwo.value !== emailOne.value){
-        emailTwo.setCustomValidity("Emails do not match!");
-    } else {
-        emailTwo.setCustomValidity("");
-    }
-  });
+  if (emailTwo.validity.typeMismatch) {
+    emailTwo.setCustomValidity("Not a valid email address!");
+  } else if (emailTwo.value !== emailOne.value) {
+    emailTwo.setCustomValidity("Emails do not match!");
+  } else {
+    emailTwo.setCustomValidity("");
+  }
+});
